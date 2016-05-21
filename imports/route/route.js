8b6687aa6d7828b05f6cba91b0dbaeda961416a1
Karan Jakhar <jakharkaran94@gmail.com>
2016-05-21 13:18:44 +0530

    named router

--- imports/route/route.js
+++ imports/route/route.js
@@ -1,16 +1,21 @@
 Router.route('/', function () {
   this.render('home');
+  name: 'home'
 });
 
 Router.route('/register', function () {
   this.render('register');
+  name: 'register'
 });
 
 Router.route('/upload', function () {
   this.render('upload');
+  name: 'upload'
 });
 
 Router.configure({
     layoutTemplate: 'main'
+},{
+	name: 'main'
 });
 
