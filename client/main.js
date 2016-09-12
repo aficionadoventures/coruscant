c138ee073bdba45b3a880f2bc0b53d0b0285692a
Hatim Kanchwala <hatim@hatimak.me>
2016-09-12 15:22:22 +0530

    Create new UI branch

--- client/main.js
+++
@@ -1,9 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-
-import '../imports/route/route.js';
-import '../imports/ui/body.js';
-import '../imports/ui/register.js';
-import '../imports/api/userdata.js';
-
-import './product.js';
